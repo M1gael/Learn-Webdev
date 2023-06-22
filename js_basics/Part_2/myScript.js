17046725897c72b5862470e4ee55d442ed81cfe8
‘M1gael’ <smit.migael.johannes@gmail.com>
2023-06-22 02:03:42 +0200

    Part2 done , basic syntacs and outputs

---
+++ js_basics/Part_2/myScript.js
@@ -0,0 +1,13 @@
+document.getElementById("innerHtml").innerHTML = 5 + 6;
+
+function documentWrite(){
+    document.write(5 + 6);
+}
+
+function windowAlert() {
+    window.alert(5 + 6);
+}
+
+function consoleLog(){
+    console.log(5 + 6);
+}
